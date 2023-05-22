c67f9151f7e0c8fe24c0b02134692fea65572f67
Beniamin Brzeziński <benek9311@wp.pl>
2023-05-22 15:36:27 +0200

    Initial changes

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import { Component } from 'react';
+import Section from './Section/Section';
+import Form from './Form/Form';
+import Contacts from './Contacts/Contacts';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  setContact = newContact => {
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+  };
+
+  deleteContact = e => {
+    const deletedId = e.currentTarget.dataset.id;
+
+    this.setState(({ contacts }) => {
+      const filteredContacts = contacts.filter(({ id }) => id !== deletedId);
+      return { contacts: filteredContacts };
+    });
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <Section>
+        <Form contacts={contacts} setContact={this.setContact} />
+        <Contacts contacts={contacts} deleteContact={this.deleteContact} />
+      </Section>
+    );
+  }
+}
+
+export default App;
